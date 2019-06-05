13cd105f52d2102f15b1a6a6f00b59cf186378b7
John Bretherton <270022735@bun2-206-10.local>
2019-06-06 10:59:24 +1200

    Formative 2.2 working with lists

---
+++ js/script.js
@@ -0,0 +1,100 @@
+console.log("js is loading");
+
+var bandList = [
+    {
+    id: 1,
+    name: 'Nearvana',
+    photo: 'nearvana.jpg',
+    genre: ['rock', 'punk', 'alternative'],
+    venue: 'San Fran',
+    price: '$35'
+    },
+    {
+    id: 2,
+    name: 'Proxy Music',
+    photo: 'proxyMusic.jpg',
+    genre: ['rock', 'glam'],
+    venue: 'Pyramid Club',
+    price: '$40'
+    },
+    {
+    id: 3,
+    name: 'Lez Zeppelin',
+    photo: 'lezZeppelin.jpg',
+    genre: ['hard rock', 'rock'],
+    venue: 'Valhalla',
+    price: '$42'
+    },
+    {
+    id: 4,
+    name: 'Oasish',
+    photo: 'oasish.jpg',
+    genre: ['rock'],
+    venue: 'Michael Fowler Center',
+    price: '$65'
+    },
+    {
+    id: 5,
+    name: 'Bjorn Again',
+    photo: 'bjornAgain.jpg',
+    genre: ['pop'],
+    venue: 'Michael Fowler Centre',
+    price: '$80'
+    },
+    {
+    id: 6,
+    name: 'Motorheadache',
+    photo: 'motorheadache.jpg',
+    genre: ['hard rock', 'metal', 'rock'],
+    venue: 'Valhalla',
+    price: '$45'
+    },
+    {
+    id: 7,
+    name: 'The Jimi Hendrix Experience Experience',
+    photo: 'jimiHendrix.jpg',
+    genre: ['rock', 'hard rock'],
+    venue: 'TSB Arena',
+    price: '$50'
+    },
+    {
+    id: 8,
+    name: 'Faux Fighters',
+    photo: 'fauxFighters.jpg',
+    genre: ['rock', 'punk', 'alternative'],
+    venue: 'Valhalla',
+    price: '$55'
+    },
+    {
+    id: 9,
+    name: 'Chicks with Dixies',
+    photo: 'chicksDixies.jpg',
+    genre: ['pop', 'alternative'],
+    venue: 'San Fran',
+    price: '$25'
+    },
+    {
+    id: 10,
+    name: 'Ringo Deathstarr',
+    photo: 'ringoDeathStarr.jpg',
+    genre: ['rock', 'alternative'],
+    venue: 'Pyramid Club',
+    price: '$35'
+    },
+    {
+    id: 11,
+    name: 'Harmonica Lewinski',
+    photo: 'harmonicaLewinski.jpg',
+    genre: ['pop', 'alternative'],
+    venue: 'Moon',
+    price: '$20'
+    },
+    {
+    id: 12,
+    name: 'The Gaza Strippers',
+    photo: 'gazaStripppers.jpg',
+    genre: ['rock', 'punk', 'alternative'],
+    venue: 'San Fran',
+    price: '$30'
+    }
+];
