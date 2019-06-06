c84b6fbb496ad02033370d4bbc77d9d074f05fb7
John Bretherton <270022735@bun2-206-10.local>
2019-06-07 11:55:51 +1200

    No progress

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
-// console.log("js is loading");
-
-var showList = [
+var shows = [
     {
     id: 1,
     bandname: 'Nearvana',
@@ -111,47 +109,72 @@ var showList = [
     }
 ];
 
-var venueList = [];
-var genreList = [];
-var gigList = [];
+// var venueList = [];
+// var genreList = [];
+// var gigList = [];
 
-$( ".venueClass" ).click(function() {
-   for (var i = 0; i < showList.length; i++) {
-     if(showList[i].venue === 'Michael Fowler Centre'){
-       gigList += showList[i].bandname;
-       console.log("MFC is hosting " + gigList);
+// FUNCTION TO SHOW LIST OF SHOWS AT SELECTED VENUE; A MODAL POP-UP SHOWING CARD(S) FOR EACH LIVE SHOW AT THAT VENUE.
+var venueShows = [];
+function venueModalFunction(){
+    for (var i = 0; i < shows.length; i++) {
+        if(this.text === shows[i].venue){
+            venueShows += shows[i];
+        };
+    };
+    console.log(venueShows);
+};
 
-     }
 
-     // console.log(showList[i].venue);
-   }
+// $('.venueClass').click(function() {
+//     for (var i = 0; i < shows.length; i++) {
+//         if($(this).text === shows[i].venue){
+//             console.log(shows[i]);
+//         }
+// }
+// });
 
-});
-function modalFunction() {
-  document.getElementById('mymodal').style.display = 'block';
 
-  // ADD SELECTED CARDS HERE
-  for (var i = 0; i < showList.length; i++) {
+// $( ".venueClass" ).click(function() {
+//    for (var i = 0; i < shows.length; i++) {
+//      if(shows[i].venue === 'Michael Fowler Centre'){
+//        gigList += shows[i].bandname;
+//        console.log("MFC is hosting " + gigList);
+//
+//      }
+//
+//      // console.log(shows[i].venue);
+//    }
+//
+// });
 
-     if(showList[i].venue === 'Michael Fowler Centre'){
-        $('#cardContainer').html += '<div class="card" style="width: 18rem;">';
-        $('#cardContainer').html +=' <img class="card-img-top" src="images/' + showList[i].photo + '" alt="Card image cap">';
-        $('#cardContainer').html += '<div class="card-body">';
-        $('#cardContainer').html += '<h5 class="card-title">' + showList[i].bandname + '</h5>';
-        $('#cardContainer').html += '<p class="card-text">' + showList[i].genre + '</p></div> </div>';
-        }
-      }
 
-}
 
-function closeFunction(){
-  $('#mymodal').style.display="none";
-}
+// MORE EFFORTS TO MAKE POP-UP CARDS FOR EACH LIVE SHOW
+
+// function venueModalFunction() {
+//   document.getElementById('venueModal').style.display = 'block';
+//
+//   // ADD SELECTED CARDS HERE
+//   for (var i = 0; i < shows.length; i++) {
+//
+//      if(shows[i].venue === 'Michael Fowler Centre'){
+//         $('#cardContainer').html += '<div class="card" style="width: 18rem;">';
+//         $('#cardContainer').html +=' <img class="card-img-top" src="images/' + shows[i].photo + ' "alt="Card image cap">';
+//         $('#cardContainer').html += '<div class="card-body">';
+//         $('#cardContainer').html += '<h5 class="card-title">' + shows[i].bandname + '</h5>';
+//         $('#cardContainer').html += '<p class="card-text">' + shows[i].genre + '</p></div> </div>';
+//         }
+//       }
+// };
+
+
+// MORE EFFORTS TO MAKE POP-UP CARDS FOR EACH LIVE SHOW
+
 // function showMoreShow(showNumber){
 //   var singleShow;
-//     for (var i = 0; i < showList.length; i++) {
-//         if (showList[i].id === showNumber) {
-//             singleShow = showList[i];
+//     for (var i = 0; i < shows.length; i++) {
+//         if (shows[i].id === showNumber) {
+//             singleShow = shows[i];
 //             break;
 //         }
 //     }
@@ -170,21 +193,50 @@ function closeFunction(){
 //               if(singleShow.genre[i] === 'Rock'){
 //                 badgeClass = 'badge-primary';
 //             } else if(singleShow.genre[i] === 'Hard rock'){
-//                 badgeClass = 'badge-success';
-//             } else if(singleShow.genre[i] === 'Pop'){
-//                 badgeClass = 'badge-danger';
-//             } else if(singleShow.genre[i] === 'Alternative'){
 //                 badgeClass = 'badge-secondary';
+//             } else if(singleShow.genre[i] === 'Pop'){
+//                 badgeClass = 'badge-info';
+//             } else if(singleShow.genre[i] === 'Alternative'){
+//                 badgeClass = 'badge-success';
 //             } else if(singleShow.genre[i] === 'Punk'){
 //                 badgeClass = 'badge-warning';
 //             } else if(singleShow.genre[i] === 'Metal'){
-//                 badgeClass = 'badge-info';
-//             } else if (singleShow.genre[i] === 'Glam'){
 //                 badgeClass = 'badge-dark';
+//             } else if (singleShow.genre[i] === 'Glam'){
+//                 badgeClass = 'badge-danger';
 //             }
 //                 $('#bandGenre').html += '<span class= "badge badge-'+genreColour+' mr-1">' + singleShow.genre[i] + '</span>';
 //             };
 //
-//         $('#showPopUp').style.display = 'flex';
+//         $('#displayList').style.display = 'flex';
 //         document.body.style.overflow = 'hidden';
 // }
+
+
+// // MORE EFFORTS TO MAKE POP-UP CARDS FOR EACH LIVE SHOW
+
+// var showCard = '<div class="card" style="width: 18rem;">';
+//         showCard += '<img src="images/'+shows.photo+'" class="card-img-top" alt="">';
+//         showCard += '<div class="card-body">';
+//             showCard += '<h5 class="card-title">'+shows.bandname+'</h5>';
+//             showCard += '<p class="card-text">Band bio Lorem ipsum dolor sit amet, consecteturnt ut la inim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>';
+//         showCard += '</div>';
+//         showCard += '<ul class="list-group list-group-flush">';
+//             showCard += '<li class="list-group-item">Cras justo odio</li>';
+//             showCard += '<li class="list-group-item">Dapibus ac facilisis in</li>';
+//             showCard += '<li class="list-group-item">Vestibulum at eros</li>';
+//         showCard += '</ul>';
+//     showCard += '</div>';
+//
+//
+//
+// var showCard = '<div class="col-4 mb-3 text-center">';
+//         showCard += '<div class="showThumb card h-100 border-'+genreClass+' " onclick="showMoreShow('+movie.id+');">';
+//             showCard += '<img src="images/'+shows.photo+'" class="card-img-top" alt="">';
+//               showCard += '<div class="card-body">';
+//                 showCard += '<h5 class="card-title">'+.bandname+'</h5>';
+//               showCard += '</div>';
+//         showCard += '</div>';
+//     showCard += '</div>';
+// // console.log(showCard);
+// displayList.innerHTML += showCard;
